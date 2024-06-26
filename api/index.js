4b1d48aad458f442676010826b130b0c0ec26aac
Naveen R <naveenramesh2003@gmail.com>
2024-06-26 17:00:58 +0530

    Added DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.Mongo_URL)
+.then(() => {
+    console.log('Connected to MongoDB!');
+}).catch(() => {
+    console.log('Connection failed!');
+});
 
 const app = express();
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
