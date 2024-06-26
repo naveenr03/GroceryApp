c7752fe3282b18520053f689c729fb4479040565
Naveen R <naveenramesh2003@gmail.com>
2024-06-26 13:26:33 +0530

    added flow-bite and Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import Home from "./pages/Home"
 import SignIn from   "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
-
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/cart" element={<Cart/>}/>
     </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+import { Navbar, TextInput,Button } from "flowbite-react";
+import { Link,useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+
+export default function Header() {
+
+    const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">  
+    
+        <Navbar.Brand>
+            <Link to="/" className="text-xl font-bold">Grocery Shop</Link>
+        </Navbar.Brand>
+        <form>
+            <TextInput type="text" placeholder='Search...' rightIcon={AiOutlineSearch} className="hidden lg:inline " />
+        </form>
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill >
+           <AiOutlineSearch/>
+        </Button>
+        <div className="flex gap-2 md:order-2">
+            <Button  className="w-12 h-10 hidden sm:inline" color="grey" pill>
+                <FaMoon/>
+            </Button>
+            <Link to="/signin" className="text-xl font-bold">
+                <Button  gradientDuoTone='purpleToBlue' pill>
+                    Sign In
+                </Button>
+            </Link>
+            <Navbar.Toggle/>
+        </div>
+        <Navbar.Collapse>
+                <Navbar.Link active={path === "/"}  as={'div'}>
+                    <Link to="/" >
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/dashboard"} as={'div'}>
+                    <Link to="/dashboard" >
+                        Products
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/cart"} as={'div'}>
+                    <Link to="/cart" >
+                        Cart
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+     </Navbar>
+  )
+}
